fc48063ac1543667a14b5d4fba44c707553ab229
mrgossplay0-star <mrgossplay0@gmail.com>
2026-08-08 01:47:12 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyALicCK2fMKzxaSVLowvgekg5nzVO5ZoRQ",
+  authDomain: "xdata-v36.firebaseapp.com",
+  databaseURL: "https://xdata-v36-default-rtdb.firebaseio.com",
+  projectId: "xdata-v36",
+  storageBucket: "xdata-v36.firebasestorage.app",
+  messagingSenderId: "34087576923",
+  appId: "1:34087576923:web:e2344c1a4075c9ed36a67e",
+  measurementId: "G-2HCLKE9JYK"
+};
